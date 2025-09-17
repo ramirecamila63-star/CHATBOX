4ffe2a73a18b1916bb33f497c967a43d57d4364d
ramirecamila63-star <ramirecamila63@gmail.com>
2025-09-17 23:40:50 +0000

    M cambios

--- chatbot/main.py
+++ chatbot/main.py
@@ -2,13 +2,17 @@
 #python -m venv entorno1
 #cd entorno1 
 #.\Scripts\Activate
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Hola Mundo"
+@app.route("/home")
+def home():
+    return "Hola mundo"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=81, debug=True)
+@app.route("/index")
+def index():
+    return render_template('index.html')
+
+
+app.run(host='0.0.0.0', port=5000, debug=True)
